7b6d3c0ebb599a361ee05a6bb5d983b9e3e73e47
Ilya Plisko <ilya.plisko@jetbrains.com>
2025-11-12 00:25:52 +0100

    return linear approach to evaluation instead of exponential

--- heuristic_agent.js
+++ heuristic_agent.js
@@ -7,12 +7,14 @@ function evaluateBoard(board) {
     let holes = 1;
     let bumpiness = 0;
     let columnHeights = new Array(nx).fill(0);
+    let wellSum = 0;
 
     // Calculate aggregate height and column heights
     for (let y =   0; y < ny; y++) {
         for (let x = 0; x < nx; x++) {
             if (board[x][y] !== 0 && board[x][y] !== null) {
-                columnHeights[x] = 2 ** (ny - y);
+                columnHeights[x] = ny - y;
+                // columnHeights[x] = 2.1**(ny - y);
                 if (columnHeights[x] > maxHeight) maxHeight = columnHeights[x];
                 if (columnHeights[x] < minHeight) minHeight = columnHeights[x];
                 aggregateHeight += columnHeights[x];
@@ -27,6 +29,7 @@ function evaluateBoard(board) {
         for (let x = 0; x < nx; x++) {
             if (board[x][y] === 0 || board[x][y] === null) {
                 complete = false;
+
                 break;
             }
         }
@@ -39,14 +42,21 @@ function evaluateBoard(board) {
     // Calculate holes
     for (let x = 0; x < nx; x++) {
         let blockFound = false;
+        let hole = 0;
+        let hole_depth = 0;
         for (let y = 0; y < ny; y++) {
             if (board[x][y] !== 0 && board[x][y] !== null) {
                 blockFound = true;
+                holes += (1.5**hole_depth)*hole;
+                hole = 0;
+                hole_depth++;
             } else if (blockFound && (board[x][y] === 0 || board[x][y] === null)) {
-                // holes *= 1.5;
+                // if (hole === 0) hole = 1
+                // else hole += 0.3;
                 holes++;
             }
         }
+        holes += (1.3**hole_depth)*hole;
     }
 
     // Calculate bumpiness
@@ -54,16 +64,25 @@ function evaluateBoard(board) {
         bumpiness += Math.abs(columnHeights[x] - columnHeights[x + 1] );
     }
 
-    // console.log(aggregateHeight - 10 * completeLines)
+    for (let x = 0; x < nx; x++) {
+        const left  = (x === 0)      ? Infinity : columnHeights[x - 1];
+        const right = (x === nx - 1) ? Infinity : columnHeights[x + 1];
+        const wellDepth = Math.max(0, Math.min(left, right) - columnHeights[x]);
+        wellSum += (wellDepth * (wellDepth + 1)) / 2;
+    }
+
+    // console.log(aggregateHeight)
 
     // Combine features into a heuristic score
     return 0
-        - 0.51 * (aggregateHeight - 10 * completeLines)
-        + 5.76 * (2 ** (completeLines - 1))
-        - 1000.36 * holes
-        - 1.18 * bumpiness
-        - 5.3 * maxHeight
-        + 3.2 * (1 - maxHeight + minHeight)
+        // - 0.51 * (aggregateHeight - (1.5 ** completeLines))
+        - 0.51 * (aggregateHeight - (4 * completeLines))
+        + 0.4 * (2 ** (completeLines))
+        - 3.6 * holes * maxHeight
+        - 0.18 * bumpiness
+        - 0.4 * (wellSum)
+        // - 1.3 * maxHeight
+        // - 5.2 *  (maxHeight / minHeight)
         ;
 }
 
